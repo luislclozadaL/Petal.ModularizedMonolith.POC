4ce60f21eeb54387cf587f240f2968dc902b864a
Luis Lozada <luis.lozada@live.com>
2022-05-02 10:00:52 -0400

    Test commit
    test

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   #   mount app.engine => '/', as: app.gem_name
   # end
 
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
   
