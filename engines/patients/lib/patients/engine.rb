c3b938a496856404390aee2ea077e66e11a87451
Ben Lopez <blopezgutierrez@petalmd.com>
2022-05-12 20:39:30 -0400

    commands,bus and facade move into common

--- engines/patients/lib/patients/engine.rb
+++ engines/patients/lib/patients/engine.rb
@@ -2,11 +2,10 @@ module Patients
   class Engine < ::Rails::Engine
     isolate_namespace Patients
 
-    
-    config.autoload_paths += %W(#{Rails.root}/common)
-    config.autoload_paths += %W(#{Rails.root}/app/orchestrator)
-    config.autoload_paths += %W(#{Rails.root}/bus)
     config.autoload_paths << File.expand_path("../../../../common", __dir__)
-    config.autoload_paths << File.expand_path("../../../../bus", __dir__)
+    config.autoload_paths << File.expand_path("../../../../common/commands", __dir__)
+    config.autoload_paths << File.expand_path("../../../../common/bus", __dir__)
+    config.autoload_paths << File.expand_path("../../../../common/facade", __dir__)
+
   end
 end
--- engines/schedule/lib/schedule/engine.rb
+++ engines/schedule/lib/schedule/engine.rb
@@ -3,9 +3,8 @@ module Schedule
     isolate_namespace Schedule
 
     config.autoload_paths << File.expand_path("../../../../common", __dir__)
-    config.autoload_paths << File.expand_path("../../../../bus", __dir__)
-    config.logger = Logger.new(STDOUT)
-    Rails.logger = Logger.new(STDOUT)
-    config.log_level = :debug
+    config.autoload_paths << File.expand_path("../../../../common/commands", __dir__)
+    config.autoload_paths << File.expand_path("../../../../common/bus", __dir__)
+    config.autoload_paths << File.expand_path("../../../../common/facade", __dir__)
   end
 end
